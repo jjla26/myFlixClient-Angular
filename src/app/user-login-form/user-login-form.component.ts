4fb211270bae94a10cbd0563a18433897138e727
Julio Lopez <julioj.lopeza@gmail.com>
2021-06-23 10:35:18 +0200

    Added actions after a successful login

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
@@ -20,6 +21,7 @@ export class UserLoginFormComponent implements OnInit {
 
   constructor(
     public fetchApiData: UserLoginService,
+    public router: Router,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar) { }
 
@@ -29,10 +31,13 @@ export class UserLoginFormComponent implements OnInit {
   // This is the function responsible for sending the form inputs to the backend
   login(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-    this.dialogRef.close(); // This will close the modal on success!
-    this.snackBar.open(result.message, 'OK', {
-        duration: 2000
-    });
+      localStorage.setItem('user', result.data.Username)
+      localStorage.setItem('token', result.token)
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open(result.message, 'OK', {
+          duration: 2000
+      });
+      this.router.navigate(['movies']);
     }, (result) => {
       this.snackBar.open(result, 'OK', {
         duration: 2000
