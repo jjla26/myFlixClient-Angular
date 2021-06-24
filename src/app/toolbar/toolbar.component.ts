3c27be69d06a5886cd46a08da30b340a7db46bfb
Julio Lopez <julioj.lopeza@gmail.com>
2021-06-24 15:59:55 +0200

    Changed toolbard to use user name

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-
+  @Input() user: string = ''
+  
   constructor() { }
 
   ngOnInit(): void {
