9d330564e98681e863351baaa8fd2165e39e14e2
Julio Lopez <julioj.lopeza@gmail.com>
2021-06-24 15:59:22 +0200

    Refactored movie card to use movie list component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { GetMoviesService } from '../fetch-api-data.service'
+import { GetMoviesService, GetUserService, AddFavoriteService, DeleteFavoriteService } from '../fetch-api-data.service'
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -8,17 +10,41 @@ import { GetMoviesService } from '../fetch-api-data.service'
 })
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
+  user: any = {};
 
-  constructor(public fetchApi: GetMoviesService) { }
+  constructor(
+    public fetchMovies: GetMoviesService,
+    public fetchUser: GetUserService,
+    public addFavorite: AddFavoriteService,
+    public deleteFavorite: DeleteFavoriteService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.getMovies()
   }
 
   getMovies(): void{
-    this.fetchApi.getMovies().subscribe((resp: any) => {
+    this.fetchMovies.getMovies().subscribe((resp: any) => {
+      const name = localStorage.getItem('user')
+      this.fetchUser.getUser(name ? name : '').subscribe(result => {
+        this.user = result.data
+      })
       this.movies = resp.data
       return this.movies
+    })
+  }
+
+  deleteFromFavorites(id: string): void{
+    this.deleteFavorite.deleteFavorite({ user: this.user.Username, movie: id}).subscribe(result => {
+      this.user = result.data
+      this.snackBar.open(result.message, 'OK', {
+        duration: 2000
+      })
+    }, error => {
+      this.snackBar.open(error.message, 'OK', {
+        duration: 2000
+      })
     })
   }
 
