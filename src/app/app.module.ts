730c6cacad969a8ab5e0d843b40bd904bf57d31c
Julio Lopez <julioj.lopeza@gmail.com>
2021-06-24 16:02:33 +0200

    Added new components to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { HttpClientModule } from '@angular/common/http'
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 
 import { RouterModule, Routes } from '@angular/router';
@@ -17,11 +20,19 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { GenreViewComponent } from './genre-view/genre-view.component';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { DescriptionViewComponent } from './description-view/description-view.component';
+import { FavoritesViewComponent } from './favorites-view/favorites-view.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfileViewComponent },
+  { path: 'favorites', component: FavoritesViewComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
@@ -31,7 +42,14 @@ const appRoutes: Routes = [
     UserRegistrationFormComponent,
     UserLoginFormComponent,
     MovieCardComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    ToolbarComponent,
+    GenreViewComponent,
+    DirectorViewComponent,
+    DescriptionViewComponent,
+    FavoritesViewComponent,
+    ProfileViewComponent,
+    MovieListComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
@@ -45,7 +63,10 @@ const appRoutes: Routes = [
     MatInputModule,
     MatSnackBarModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
