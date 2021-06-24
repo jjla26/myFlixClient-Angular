1403fade4c010100ef8e0303a144ced1a0b802eb
Julio Lopez <julioj.lopeza@gmail.com>
2021-06-24 12:16:32 +0200

    Added director component

---
+++ src/app/director-view/director-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-director-view',
+  templateUrl: './director-view.component.html',
+  styleUrls: ['./director-view.component.scss']
+})
+export class DirectorViewComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {name: string, bio: string} ) { }
+
+  ngOnInit(): void {
+  }
+
+}
